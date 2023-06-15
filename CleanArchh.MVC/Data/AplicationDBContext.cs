08dc6c4f349160844290e471f69dc19790d7fcf6
Ezzelden <ezz@spark-sys.com>
2023-06-15 15:59:00 +0300

    Adding Structure

---
+++ CleanArchh.MVC/Data/AplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchh.MVC.Data
+{
+    public class AplicationDBContext :IdentityDbContext
+    {
+
+        public AplicationDBContext(DbContextOptions<AplicationDBContext>options):base  (options) { }    
+     
+    }
+}
