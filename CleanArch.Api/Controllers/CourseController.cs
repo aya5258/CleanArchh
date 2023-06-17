129061f947f72e12032d33973792d9ee495fe1d9
iti <iti@DESKTOP-15JLN9H>
2023-06-17 05:46:16 -0700

    Add Api&Rename Controller

---
+++ CleanArch.Api/Controllers/CourseController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+
+
+    
+     
+    }
+}
